b556ecf6f5953f925bf2cece171eab77a047accc
Arnaud Beaudelet <beaudele@gmail.com>
2016-12-06 17:51:53 -0500

    Beautify code.

--- src/main/java/com/sandbox/springmvc/service/UserService.java
+++ src/main/java/com/sandbox/springmvc/service/UserService.java
@@ -1,27 +1,71 @@
 package com.sandbox.springmvc.service;
 
-import java.util.List;
-
 import com.sandbox.springmvc.model.RegisteredUser;
 
+import java.util.List;
 
 
+
+/**
+ * The Interface UserService.
+ */
 public interface UserService {
-	
-	RegisteredUser findById(Long id);
-	
-	RegisteredUser findByName(String name);
-	
-	void saveUser(RegisteredUser user);
-	
-	void updateUser(RegisteredUser user);
-	
-	void deleteUserById(Long id);
 
-	List<RegisteredUser> findAllUsers(); 
-	
-	void deleteAllUsers();
-	
-	public boolean isUserExist(RegisteredUser user);
-	
+  /**
+   * Find by id.
+   *
+   * @param id the id
+   * @return the registered user
+   */
+  RegisteredUser findById(Long id);
+
+  /**
+   * Find by name.
+   *
+   * @param name the name
+   * @return the registered user
+   */
+  RegisteredUser findByName(String name);
+
+  /**
+   * Save user.
+   *
+   * @param user the user
+   */
+  void saveUser(RegisteredUser user);
+
+  /**
+   * Update user.
+   *
+   * @param user the user
+   */
+  void updateUser(RegisteredUser user);
+
+  /**
+   * Delete user by id.
+   *
+   * @param id the id
+   */
+  void deleteUserById(Long id);
+
+  /**
+   * Find all users.
+   *
+   * @return the list
+   */
+  List<RegisteredUser> findAllUsers();
+
+  /**
+   * Delete all users.
+   */
+  void deleteAllUsers();
+
+  /**
+   * Checks if is user exist.
+   *
+   * @param user the user
+   * @return true, if is user exist
+   */
+  public boolean isUserExist(RegisteredUser user);
+
 }
